33f3cbf744b07ae4bd4bd23aad5d58ae3b498de6
Hardik Patel <patelhardikp6@gmail.com>
2025-09-05 14:51:18 +0530

    chnage_gitignore

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx'
 import {Amplify} from 'aws-amplify'
-import awsconfig from './aws-exports.js'
+import awsmobile from './aws-exports.js'
 
-Amplify.configure(awsconfig);
+Amplify.configure(awsmobile);
 
 createRoot(document.getElementById('root')).render(
     <AuthProvider>
