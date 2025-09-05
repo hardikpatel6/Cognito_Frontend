84cf738c009575b98274379a0c80d372bb8aad72
Hardik Patel <patelhardikp6@gmail.com>
2025-09-05 14:46:57 +0530

    add_aws-export.js

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -2,9 +2,15 @@
 import React from 'react'
 
 const DashBoard = () => {
+  const handleLogOut = () => {
+    // Logic for logging out the user
+    console.log("User logged out");
+  }
   return (
     <div>
       <h2>Hello World and Hello Hardik</h2>
+      <button
+      onClick={handleLogOut}>SignOut</button>
     </div>
   )
 }
