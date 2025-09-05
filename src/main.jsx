6782faf7b096e793178ba20caf9f31d88531ca70
Hardik Patel <patelhardikp6@gmail.com>
2025-09-05 13:27:28 +0530

    add_name_export

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import {Amplify} from "aws-amplify"
 import { AuthProvider } from './contexts/AuthContext.jsx'
-import awsconfig from "./aws-exports";
+import {awsconfig} from "./aws-exports";
 
 Amplify.configure(awsconfig);
 
